63a605c6e26ad91694d815111a241e20db9cc268
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-20 23:35:49 +0600

    project final

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to='/home'><h1>Sundarban eXpress</h1></Link>
+                <Link className="navbar-brand" to='/'><h1>Sundarban eXpress</h1></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
